82bc01f62938611db240058559188860e3ddf8a1
Thays Silva <113531216+Profthayssilva@users.noreply.github.com>
2023-09-15 17:24:26 -0300

    Add files via upload

---
+++ activity1.js
@@ -0,0 +1,16 @@
+// // Primeiro, definiremos a variável score com valor = 0;
+
+// Então vamos escrever a função updateScore.
+
+// Em seguida, atualize o elemento HTML que tem seu id como "score", com o texto “Pontuação: =
+// score + 1 (esta é a variável).
+
+// vamos escrever a função saveScore.
+
+// vamos escrever a função nextPage:
+
+
+
+
+
+
